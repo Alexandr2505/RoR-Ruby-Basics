800a1bc53eadf4eb0cba54b7eb75bd5bb2574c4b
Alexandr2505 <56608312+Alexandr2505@users.noreply.github.com>
2019-10-29 23:53:11 +0200

    Add files via upload

---
+++ 3Lesson/transfer_to_html/lib/transfer_to_html.rb
@@ -0,0 +1,7 @@
+require "transfer_to_html/version"
+
+module TransferToHtml
+  class Error < StandardError; end
+  def TransferToHtml
+  	print "Hellow"
+end
---
+++ 3Lesson/transfer_to_html/lib/transfer_to_html/version.rb
@@ -0,0 +1,3 @@
+module TransferToHtml
+  VERSION = "0.1.0"
+end
