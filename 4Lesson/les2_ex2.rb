e8929b152d01368563c18e1a60b3d3d71b83919f
Alexandr2505 <56608312+Alexandr2505@users.noreply.github.com>
2019-11-07 17:46:44 +0200

    Add files via upload

--- 4Lesson/les2_ex2.rb
+++ 4Lesson/les2_ex2.rb
@@ -1,67 +1,80 @@
 require 'yaml'
+
 class Animal
-  attr_accessor :name
 
-  def initialize(names)
-
-    @name = names
-    @breed = "Cat"
-    @life = 12
-    @mood = 120
-    @hungry = 120
-    @sleepy = 220
-    @t = Time.new
-  end
 
   def feed
+    stat_save = YAML.load( File.read("animal.yaml"))
+    @name = stat_save[0]["name"]
+    @hungry = stat_save[0] ["hungry"]
     puts "Вы кормите #{@name}"
     @hungry += 25
-    $life = 155
+    status = ["hungry" => @hungry]
+    File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+    puts @hungry
   end
 
   def putToBed
     puts "Вы укладываете #{@name} спать."
-    3.times {if @sleepy <= 90
-        time_is_coming
-      end}
-
       if @sleepy <= 90
-        puts "#{@breed} : #{@name} крепко спит."
-        @sleepy += 80
-        @hungry -= 5
-        puts "#{@breed} : #{@name} медленно просыпается."
+            stat_save = YAML.load( File.read("animal.yaml"))
+            @name = stat_save[0]["name"]
+            @hungry = stat_save[0] ["hungry"]
+            puts "#{@name} крепко спит."
+            @sleepy += 80
+            @hungry -= 5
+            puts "#{@name} медленно просыпается."
+            File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+            puts @hungry
       end
     if @sleepy > 90
-      puts "#{@breed}: #{@name} не хочет спать. Он бодр!"
+       stat_save = YAML.load( File.read("animal.yaml"))
+       @name = stat_save[0]["name"]
+       @hungry = stat_save[0] ["hungry"]
+       puts "Вы кормите #{@name}"
+       puts "#{@name} не хочет спать. Он бодр!"
     end
   end
 
    def walk
+    stat_save = YAML.load( File.read("animal.yaml"))
+    @name = stat_save[0]["name"]
+    @hungry = stat_save[0] ["hungry"]
+    puts "Вы кормите #{@name}"
     puts "Вы выгуливаете #{@name}"
     @mood += 15
     @hungry -= 10
     @sleepy -= 5
-    time_is_coming
+    File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+    puts @hungry
   end
 
   def toss
-    puts "Вы играете с  #{@name}" 
+    stat_save = YAML.load( File.read("animal.yaml"))
+    @name = stat_save[0]["name"]
+    @hungry = stat_save[0] ["hungry"]
+    puts "Вы кормите #{@name}"
+    puts "Вы играете с #{@name}" 
     puts 'Он радуеться, как маленкий ребёнок!'
     @mood += 20
     @hungry -=  15
     @sleepy -= 5
-    time_is_coming
+    File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+    puts @hungry
   end
 
   def wash
+    stat_save = YAML.load( File.read("animal.yaml"))
+    @name = stat_save[0]["name"]
+    @hungry = stat_save[0] ["hungry"]
+    puts "Вы кормите #{@name}"
     puts "#{@name} моется."
     puts 'Он радуеться -- чистоте.'
     @mood += 5
     @sleepy -= 5
-    time_is_coming
+    File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+    puts @hungry
   end
-
-
 
   def help
     puts "1 - При кормлении пополняются жизни(life+ ), и уменшаеться голод(@hungry+) и улучшается настроение(@mood+)"
@@ -70,30 +83,7 @@ class Animal
     puts "4 - При мытье увиличевается сондивость(@sleepy-) но увиличевается настроение(@mood+)"
     puts "5 - В игре увиличевается голод (@hungry- ) и сондивость(sleepy-), но увиличевается настроение(@mood)"
     puts "6 - Выход из програмы"
-    puts "* Если долго не выбтрать вариант, то при следующем выборе случаеться неожиданость(изменение характеристик)"
-    puts "В начале написать имя и вид животного (папугай, кот и т.д.) - только на англисийском"
-  end
-
-  private
-
-  def time_is_coming # проходит некоторое время
-      @hungry -= rand(6)
-      @sleepy -= rand(6)
-      @mood -= rand(4) 
-      if @t + 10 < Time.new
-        a = rand(2)
-          if a == 0
-            puts "Небеса помогают нам, показания #{@name} стали лучше"
-            @hungry += rand(14)
-            @sleepy += rand(14)
-            @mood += rand(9) 
-          else 
-            puts "Небеса против нас, показания #{@name} стали хуже"
-            @hungry -= rand(6)
-            @sleepy -= rand(6)
-            @mood -= rand(4)
-          end
-      end     
+    puts "В начале написать имя - только на англисийском"
   end
 
 end
--- 4Lesson/sinatra.rb
+++ 4Lesson/sinatra.rb
@@ -3,19 +3,41 @@ require 'yaml'
 require_relative 'les2_ex2'
 
 get '/' do
-  @breed = "Cat"
   erb :index
 end
 
 
 post '/' do
-  @name = params['name']
-  @breed = "Cat"
-  puts "Name first pet: #{@names}"
-  @animal = Animal.new(params['name'])
 
-  if params[:feed]
-    @animal.feed
+  @pets =  Animal.new
+
+  if params[:name] != "s"
+    @name = params[:name]
+    puts "Name first pet: #{@name}"
+    @life = 11
+    @mood = 101
+    @hungry = 102
+    @sleepy = 202
+    status = ["name" => @name,
+              "breed" => "Dog",
+              "life" => @life,
+              "mood" => @mood,
+              "hungry" => @hungry,
+              "sleepy" => @sleepy]
+  File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
+  end
+  
+
+  stat_save = YAML.load( File.read("animal.yaml"))
+  @name = stat_save[0]["name"]
+  @breed = stat_save[0] ["breed"]
+  @life = stat_save[0] ["life"]
+  @mood = stat_save[0] ["mood"]
+  @hungry = stat_save[0] ["hungry"]
+  @sleepy = stat_save[0] ["sleepy"]
+
+ if params[:feed]
+  @pets.feed
   elsif params[:putToBed]
     @animal.putToBed
   elsif params[:walk]
@@ -26,13 +48,5 @@ post '/' do
     @animal.wash
   end
 
-  @life = $life
-  status = ["name" => @name,
-            "breed" => @breed,
-            "life" => @life,
-            "mood" => @mood,
-            "hungry" => @hungry,
-            "sleepy" => @sleepy]
-  File.open("animal.yaml", "w") { |file| file.write(status.to_yaml) }
   erb :index
 end
